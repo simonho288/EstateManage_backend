3a0edd1ae4ea01676dba19982f7bb662eb25c6f8
simonho <simon@alslab.com>
2022-12-29 23:43:30 +0000

    Remove all code files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-export default {
-  testEnvironment: 'miniflare',
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'esbuild-jest',
-  },
-}
--- src/const.ts
+++
@@ -1,7 +0,0 @@
-
-export let Constant = {
-  MAX_BLOCKNAME_LEN: 10,
-  MAX_FLOORNAME_LEN: 10,
-  MAX_ROOMNAME_LEN: 10,
-  EMAIL_BODY_TEXT: 'The email contents are HTML format. Please view it in HTML Markup',
-}
